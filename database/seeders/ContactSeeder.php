860a05dcbec2ec8af93c7e552217f6f010e086ec
José Faro <blakmetall@gmail.com>
2024-09-25 13:46:03 -0600

    se agregaron seeds
    * se agregaron los seeds
    * se agregaron las librerías de Carbon y Faker para generar la información de los seeds

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ContactSeeder extends Seeder
+{
+    private $faker = null;
+
+    private $addressSeedLimit = 3;
+    private $contactSeedLimit = 25;
+    private $emailsSeedLimit = 2;
+    private $phonesSeedLimit = 2;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->faker = Faker::create();
+        $this->seedContacts();
+    }
+
+    private function seedContacts()
+    {
+        for ($i = 0; $i < $this->contactSeedLimit; $i++) {
+            $contact_id = DB::table('contacts')->insertGetId([
+                'name' => $this->faker->name,
+                'dateOfBirth' => $this->faker->dateTime->format('Y-m-d'),
+                'website' => $this->faker->url,
+                'company' => $this->faker->company,
+                'notes' => $this->faker->sentence,
+            ]);
+
+            $this->seedContactAddresses($contact_id);
+            $this->seedContactEmails($contact_id);
+            $this->seedContactPhones($contact_id);
+        }
+    }
+
+    private function seedContactAddresses($contact_id) {
+        for ($i = 0; $i < $this->addressSeedLimit; $i++) {
+            $baseTableValues = $this->prepareBaseContactInfoValues($contact_id, $i);
+            $data = array_merge($baseTableValues, ['address' => $this->faker->address]);
+            DB::table('contact_addresses')->insert($data);
+        }
+    }
+
+    private function seedContactEmails($contact_id) {
+        for ($i = 0; $i < $this->emailsSeedLimit; $i++) {
+            $baseTableValues = $this->prepareBaseContactInfoValues($contact_id, $i);
+            $data = array_merge($baseTableValues, ['email' => $this->faker->email]);
+            DB::table('contact_emails')->insert($data);
+        }
+    }
+
+    private function seedContactPhones($contact_id) {
+        for ($i = 0; $i < $this->phonesSeedLimit; $i++) {
+            $baseTableValues = $this->prepareBaseContactInfoValues($contact_id, $i);
+            $data = array_merge($baseTableValues, ['phone' => $this->faker->phoneNumber]);
+            DB::table('contact_phones')->insert($data);
+        }
+    }
+
+    private function prepareBaseContactInfoValues($contact_id, $iteration_counter) {
+        $now = Carbon::now()->format('Y-m-d H:i:s');
+
+        return [
+            'contact_id' => $contact_id,
+            'is_default' => $this->shouldSetDefault($iteration_counter),
+            'created_at' => $now,
+            'updated_at' => $now,
+        ];
+    }
+
+    private function shouldSetDefault($iteration_counter) {
+        $is_first_iteraion = $iteration_counter == 0;
+        return $is_first_iteraion ? 1 : 0;
+    }
+}
