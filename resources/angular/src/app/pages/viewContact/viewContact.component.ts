10bb1e5d3f92fca3c884ce13af9fe08a478c6d59
José Faro <blakmetall@gmail.com>
2024-09-26 11:34:27 -0600

    se agregó la pantalla para poder ver los detalles del contacto

--- resources/angular/src/app/pages/viewContact/viewContact.component.ts
+++ resources/angular/src/app/pages/viewContact/viewContact.component.ts
@@ -1,18 +1,38 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { ContactsService } from '../../services/contacts.service';
 import { HttpClientModule } from '@angular/common/http';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-view-contact',
   standalone: true,
-  imports: [HttpClientModule, NgFor],
+  imports: [HttpClientModule, NgFor, NgIf],
   templateUrl: './viewContact.component.html',
   styleUrl: './viewContact.component.scss',
   providers: [ContactsService, HttpClientModule],
 })
 export class ViewContactComponent {
-  constructor(private contactsService: ContactsService) {}
+  contact: any = {};
 
-  ngOnInit(): void {}
+  constructor(
+    private contactsService: ContactsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const contactId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadContact(contactId);
+  }
+
+  loadContact(contactId: number): void {
+    this.contactsService.getContact(contactId).subscribe(
+      (response) => {
+        this.contact = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
