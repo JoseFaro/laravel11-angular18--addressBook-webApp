db5e2c393a8c12a677df72471ddb43c40a02d925
José Faro <blakmetall@gmail.com>
2024-09-25 23:30:42 -0600

    componente base de formulario de contactos

---
+++ resources/angular/src/app/components/contactForm/contactForm.component.ts
@@ -0,0 +1,51 @@
+import { ButtonComponent } from '../../components/button/button.component';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [ButtonComponent, NgIf, NgFor, ReactiveFormsModule],
+  templateUrl: './contactForm.component.html',
+})
+export class ContactFormComponent {
+  @Input() contact?: any = {};
+  @Output() onSave = new EventEmitter<void>();
+
+  isCreate: boolean = false;
+  isUpdate: boolean = false;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      dateOfBirth: [''],
+      company: [''],
+      website: [''],
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['contact'] && changes['contact'].currentValue) {
+      this.isCreate = !this.contact?.id;
+      this.isUpdate = Boolean(this.contact?.id);
+      this.form.patchValue(this.contact);
+    }
+  }
+
+  handleOnSubmit(): void {
+    this.onSave.emit(this.form.value);
+  }
+}
