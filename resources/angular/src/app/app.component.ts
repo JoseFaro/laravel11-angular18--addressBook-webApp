642a7c0dedfe5753ec5f5494484391032cba3b56
José Faro <blakmetall@gmail.com>
2024-09-25 18:26:55 -0600

    importaciones de componentes en app.component

--- resources/angular/src/app/app.component.ts
+++ resources/angular/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactsComponent, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular';
